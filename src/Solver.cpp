8529bdd5aba104093816723eaf416df6099664aa
Nadpher <Nadpher@gmail.com>
2022-06-13 20:04:55 +0200

    fixed solving algorithm

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -9,6 +9,7 @@ bool Solver::solve(unsigned int index)
 {
 	Coord pos = { index % Board::boardLength, index / Board::boardLength };
 
+	// if last cell
 	if (index == (Board::boardLength * Board::boardLength) - 1)
 	{
 		if (board_.readCell(pos))
@@ -30,7 +31,10 @@ bool Solver::solve(unsigned int index)
 	{
 		if (board_.readCell(pos))
 		{
-			return solve(index + 1);
+			if (solve(index + 1))
+			{
+				return true;
+			}
 		}
 
 		for (int i = 1; i <= Board::boardLength; ++i)
@@ -39,7 +43,10 @@ bool Solver::solve(unsigned int index)
 
 			if (board_.checkCell(pos))
 			{
-				return solve(index + 1);
+				if (solve(index + 1))
+				{
+					return true;
+				}
 			}
 		}
 	}
